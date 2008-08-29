48045843214c2eb87bd24ba965b909f1bc1ad734
Scott Taylor <scott@railsnewbie.com>
2008-08-28 22:59:48 -0400

    * Adding version to the load path

--- lib/cap_db_dump.rb
+++ lib/cap_db_dump.rb
@@ -1,6 +1,7 @@
 module CapDbDump
   class << self
     def load
+      require File.dirname(__FILE__) + "/cap_db_dump/version"
       require File.dirname(__FILE__) + "/cap_db_dump/recipes"
     end
   end
