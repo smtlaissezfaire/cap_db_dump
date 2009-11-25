604ed9d96a33860834e1435e2139779bf2896a09
Scott Taylor <scott@railsnewbie.com>
2009-11-25 18:40:45 -0500

    Remove whitespace

--- recipes/cap_db_dump.rb
+++ recipes/cap_db_dump.rb
@@ -1,7 +1,7 @@
 namespace :database do
   # If you give a name for the session table, only the schema for that table will
   # be backed up. If the value is nil, disregard this option
-  set :sessions_table,     nil 
+  set :sessions_table,     nil
 
   set :dump_root_path, "/tmp"
   set :now, Time.now
@@ -16,38 +16,38 @@ namespace :database do
     def give_description(desc_string)
       puts "  ** #{desc_string}"
     end
-    
+
     def database_name
       database_yml_in_env["database"]
     end
-    
+
     def database_username
       database_yml_in_env["username"]
     end
-    
+
     def database_host
       database_yml_in_env["host"] || "localhost"
     end
-    
+
     def database_password
       database_yml_in_env["password"]
     end
-    
+
     def database_yml_in_env
       database_yml[rails_env]
     end
-    
+
     def database_yml
       @database_yml ||= read_db_yml
     end
-    
+
     def tasks_matching_for_db_dump
       { :only => { :db_dump => true } }
     end
   end
 
   extend CapDbDumpHelpers
-  
+
   task :read_db_yml, tasks_matching_for_db_dump do
     @database_yml ||= begin
       yaml = capture("cat #{shared_path}/config/database.yml")
@@ -61,7 +61,7 @@ namespace :database do
 
   task :create_dump, tasks_matching_for_db_dump do
     ignore_sessions = sessions_table ? "--ignore-table=#{database_name}.sessions" : ""
-    
+
     command = "mysqldump -u #{database_username} -h #{database_host} #{password_field} -Q "
     command << "--add-drop-table -O add-locks=FALSE --lock-tables=FALSE --single-transaction "
     command << "#{ignore_sessions} #{database_name} > #{dump_path}"
@@ -71,7 +71,7 @@ namespace :database do
     run command
     session_schema_dump if sessions_table
   end
-  
+
   task :session_schema_dump, tasks_matching_for_db_dump do
     command = "mysqldump -u #{database_username} -h #{database_host} #{password_field} "
     command << "-Q --add-drop-table --single-transaction --no-data #{database_name} sessions >> #{dump_path}"
@@ -90,7 +90,7 @@ namespace :database do
     give_description "Gzip'ing the file"
     run cmd
   end
-  
+
   desc "Make a production dump, transfer it to this machine"
   task :dump_and_transfer, tasks_matching_for_db_dump do
     dump
